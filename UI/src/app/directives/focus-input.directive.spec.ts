844452285324335c4e59965d22d070dd4b5e6d34
Conor Haining <conor.haining@gmail.com>
2020-12-05 23:00:15 +0000

    feat: make component focus

---
+++ UI/src/app/directives/focus-input.directive.spec.ts
@@ -0,0 +1,24 @@
+import { ElementRef } from '@angular/core';
+import { FocusInputDirective } from './focus-input.directive';
+
+describe('FocusInputDirective', () => {
+  it('should create an instance', () => {
+    const elementRef = new ElementRef({ focus: 'x' });
+    const directive = new FocusInputDirective(elementRef);
+
+    expect(directive).toBeTruthy();
+  });
+
+  it('should throw an error when element doesn\'t have focus', () => {
+    const elementRef = new ElementRef({});
+
+    expect(() => {new FocusInputDirective(elementRef)}).toThrow();
+  });
+
+  it('should not throw an error when element does have focus', () => {
+    const elementRef = new ElementRef({ focus: 'x' });
+
+    expect(() => {new FocusInputDirective(elementRef)}).not.toThrow();
+  });
+
+});
