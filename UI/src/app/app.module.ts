b10bad4f63f983f085c18d9aad3e100d55d87cf4
Conor Haining <conor.haining@gmail.com>
2020-12-05 20:47:37 +0000

    feat: create searchbar

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { LpHeaderComponent } from './lp-header/lp-header.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LpHeaderComponent
+    LpHeaderComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ UI/src/app/searchbar/searchbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lp-searchbar',
+  templateUrl: './searchbar.component.html',
+  styles: [
+  ]
+})
+export class SearchbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
