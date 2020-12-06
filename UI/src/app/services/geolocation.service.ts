eda24244b24154d6f26f4f33d5deae1b8152fd72
Conor Haining <conor.haining@gmail.com>
2020-12-06 15:31:28 +0000

    feat: add geolcation service

---
+++ UI/src/app/services/geolocation.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocationService {
+
+  private updateGeolocationSource = new Subject<boolean>();
+  updateGeolocation$ = this.updateGeolocationSource.asObservable();
+
+  centerMap(locate: boolean): void {
+    this.updateGeolocationSource.next(locate);
+  }
+}
