9b562c84ca921057a9116b07d7e75c9d3285fab1
Conor Haining <conor.haining@gmail.com>
2020-12-07 11:21:00 +0000

    feat: emit search values

--- UI/src/app/searchbar/searchbar.component.ts
+++ UI/src/app/searchbar/searchbar.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { FeatureCollection } from '@turf/helpers';
+import { LocationSearchService } from '../services/location-search.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'lp-searchbar',
@@ -10,19 +13,26 @@ export class SearchbarComponent implements OnInit {
   search = new FormControl('');
   showSearch = false;
 
-  constructor() { }
+  @Output() searchResults = new EventEmitter<FeatureCollection>();
+
+  constructor(private readonly locationSearch: LocationSearchService) { }
 
   ngOnInit(): void {
 
-    this.search.valueChanges.subscribe((value) => {
-      console.log(value);
-    });
+    this.search.valueChanges
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe((value) => {
+        this.locationSearch.getSearchLocations(value).subscribe((searchResults) => this.searchResults.emit(searchResults));
+      });
 
   }
 
   onBlur(): void {
-    this.showSearch = false;
-    this.clearSearch();
+    // this.showSearch = false;
+    // this.clearSearch();
+    // this.searchResults.emit(featureCollection())
   }
 
   clearSearch(): void {
