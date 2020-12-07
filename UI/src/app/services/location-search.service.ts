f7b318aa0bdd7c8c364e3a1d0f962001015abde5
Conor Haining <conor.haining@gmail.com>
2020-12-07 11:20:34 +0000

    feat: service for map fetching

---
+++ UI/src/app/services/location-search.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { featureCollection, FeatureCollection } from '@turf/helpers';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationSearchService {
+
+  private readonly baseUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/{SEARCH}.json?access_token={ACCESS_TOKEN}&';
+  private readonly geoencodingParams: HttpParams = new HttpParams({
+    fromObject: {
+      autocomplete: 'true',
+      bbox: '-10.788574,54.498758,0.109863,61.164437', // Bounding box of Scotland, roughly
+      limit: '10',
+      types: 'postcode,district,place,locality,neighborhood,address,poi'
+    }
+  });
+
+  constructor(private readonly http: HttpClient) { }
+
+
+  getSearchLocations(searchValue: string): Observable<FeatureCollection> {
+    if (!searchValue) { return of(featureCollection([])); }
+    
+    let url = this.baseUrl.replace('{SEARCH}', searchValue).replace('{ACCESS_TOKEN}', environment.map.accessToken);
+    url += this.geoencodingParams.toString();
+    return this.http.get<FeatureCollection>(url);
+  }
+}
